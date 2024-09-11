470ab6af228827010a045dc6596ed66bec61f127
beeb@neb.one <mercedesmwit20@gmail.com>
2024-09-11 15:23:36 +0700

    feat: add render promt function

---
+++ styles/color.go
@@ -0,0 +1,13 @@
+package styles
+
+type HexColor string
+
+const (
+	White  HexColor = "#FFFFFF"
+	Cyan   HexColor = "#27D8FF"
+	Green  HexColor = "#B0EE5F"
+	Gray   HexColor = "#808080"
+	Red    HexColor = "#FF5656"
+	Black  HexColor = "#000000"
+	Yellow HexColor = "#FFBE5E"
+)
--- styles/text.go
+++ styles/text.go
@@ -1,18 +1,9 @@
 package styles
 
 import (
+	"strings"
+
 	"github.com/charmbracelet/lipgloss"
-)
-
-type HexColor string
-
-const (
-	White HexColor = "#FFFFFF"
-	Cyan  HexColor = "#27D8FF"
-	Green HexColor = "#B0EE5F"
-	Gray  HexColor = "#808080"
-	Red   HexColor = "#FF5656"
-	Black HexColor = "#000000"
 )
 
 // Text applies a hex color to a given string and returns the styled string
@@ -31,8 +22,48 @@ func Cursor(cursorChar string) string {
 	cursorStyle := lipgloss.NewStyle().
 		Bold(true).                                // Make cursor bold
 		Reverse(true).                             // Reverse the foreground and background colors
-		Background(lipgloss.Color(string(Black))). // White background
-		Foreground(lipgloss.Color(string(White)))  // Black foreground
+		Background(lipgloss.Color(string(Black))). // Black background
+		Foreground(lipgloss.Color(string(White)))  // White foreground
 
 	return cursorStyle.Render(cursorChar)
+}
+
+type PromptStatus string
+
+const (
+	Empty       PromptStatus = "empty"
+	Completed   PromptStatus = "completed"
+	Question    PromptStatus = "question"
+	Information PromptStatus = "information"
+)
+
+var (
+	QuestionMark    string = Text("? ", Cyan)
+	CorrectMark     string = Text("✓ ", Green)
+	InformationMark string = Text("i ", Cyan)
+)
+
+// RenderPrompt highlights phrases in the text if they match any phrase in the highlights list
+func RenderPrompt(text string, highlights []string, status PromptStatus) string {
+	prompt := ""
+	switch status {
+	case Question:
+		prompt += QuestionMark
+	case Completed:
+		prompt += CorrectMark
+	case Information:
+		prompt += InformationMark
+	}
+
+	// Iterate over each highlight phrase and replace it in the text
+	for _, highlight := range highlights {
+		if strings.Contains(text, highlight) {
+			// Apply Cyan color to the matching highlight
+			coloredHighlight := BoldText(highlight, Cyan)
+			text = strings.ReplaceAll(text, highlight, coloredHighlight)
+		}
+	}
+
+	// Return the prompt with the highlighted text
+	return prompt + text
 }
