56695632805931b6f87783d0cd135c5163f12e44
traviolus <42636319+traviolus@users.noreply.github.com>
2024-09-20 15:26:53 +0700

    feat: weave initia log

--- cmd/initia.go
+++ cmd/initia.go
@@ -1,12 +1,25 @@
 package cmd
 
 import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"time"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 
 	"github.com/initia-labs/weave/models"
 	"github.com/initia-labs/weave/models/weaveinit"
 	"github.com/initia-labs/weave/utils"
+)
+
+const (
+	PreviousLogLines = 100
 )
 
 func InitiaCommand() *cobra.Command {
@@ -87,11 +100,70 @@ func initiaLogCommand() *cobra.Command {
 		Use:   "log",
 		Short: "Stream the logs of the initiad full node application.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO: Implement log streaming
+			userHome, err := os.UserHomeDir()
+			if err != nil {
+				return fmt.Errorf("failed to get user home directory: %v", err)
+			}
 
+			logFilePathOut := filepath.Join(userHome, utils.WeaveLogDirectory, "initia.stdout.log")
+			logFilePathErr := filepath.Join(userHome, utils.WeaveLogDirectory, "initia.stderr.log")
+
+			sigChan := make(chan os.Signal, 1)
+			signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+			go tailLogFile(logFilePathOut, os.Stdout)
+			go tailLogFile(logFilePathErr, os.Stderr)
+
+			<-sigChan
+
+			fmt.Println("Stopping log streaming...")
 			return nil
 		},
 	}
 
 	return logCmd
+}
+
+func tailLogFile(filePath string, output io.Writer) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("error opening log file %s: %v\n", filePath, err)
+		return
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+		if len(lines) > PreviousLogLines {
+			lines = lines[1:]
+		}
+	}
+
+	for _, line := range lines {
+		fmt.Fprintln(output, line)
+	}
+
+	_, err = file.Seek(0, io.SeekEnd)
+	if err != nil {
+		fmt.Printf("error seeking to end of log file %s: %v\n", filePath, err)
+		return
+	}
+
+	for {
+		var line = make([]byte, 4096)
+		n, err := file.Read(line)
+		if err != nil && err != io.EOF {
+			fmt.Printf("error reading log file %s: %v\n", filePath, err)
+			return
+		}
+
+		if n > 0 {
+			output.Write(line[:n])
+		} else {
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
