e920aa63d360c21c3155cc426f010d48b05565c4
beeb@neb.one <mercedesmwit20@gmail.com>
2024-09-26 10:29:25 +0700

    fix: add PreRunE

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,9 @@ import (
 	"context"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/initia-labs/weave/utils"
 )
 
 var Version string
@@ -13,6 +16,14 @@ func Execute() error {
 		Version: Version,
 		Use:     "weave",
 		Long:    "Weave is the CLI for managing Initia deployments.",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			viper.AutomaticEnv()
+			viper.SetEnvPrefix("weave")
+			if err := utils.InitializeConfig(); err != nil {
+				return err
+			}
+			return nil
+		},
 	}
 
 	rootCmd.AddCommand(InitCommand())
