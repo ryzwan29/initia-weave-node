9b53aaba82a7b61284919908dc29d0e1351020f7
traviolus <42636319+traviolus@users.noreply.github.com>
2024-09-30 16:49:46 +0700

    feat: minitia launch flows

--- models/minitia/launch.go
+++ models/minitia/launch.go
@@ -55,7 +55,7 @@ func (m *ExistingMinitiaChecker) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	m.loading = loader
 	if m.loading.Completing {
 		if !m.state.existingMinitiaApp {
-			// TODO: Continue
+			return NewNetworkSelect(m.state), nil
 		} else {
 			return NewDeleteExistingMinitiaInput(m.state), nil
 		}
@@ -64,7 +64,8 @@ func (m *ExistingMinitiaChecker) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m *ExistingMinitiaChecker) View() string {
-	return styles.Text("🪢 For launching Minitia, once all required configurations are complete, \nit will run for a few blocks to set up neccesary components.\nPlease note that this may take a moment, and your patience is appreciated!\n\n", styles.Ivory) + m.loading.View()
+	return styles.Text("🪢 For launching Minitia, once all required configurations are complete, \nit will run for a few blocks to set up neccesary components.\nPlease note that this may take a moment, and your patience is appreciated!\n\n", styles.Ivory) +
+		m.loading.View()
 }
 
 type DeleteExistingMinitiaInput struct {
@@ -96,7 +97,7 @@ func (m *DeleteExistingMinitiaInput) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	input, cmd, done := m.TextInput.Update(msg)
 	if done {
 		// TODO: Delete .minitia folder
-		return NewExistingAppReplaceSelect(m.state), nil
+		return NewNetworkSelect(m.state), nil
 	}
 	m.TextInput = input
 	return m, cmd
@@ -121,14 +122,14 @@ var (
 	Mainnet NetworkSelectOption = ""
 )
 
-func NewExistingAppReplaceSelect(state *LaunchState) *NetworkSelect {
+func NewNetworkSelect(state *LaunchState) *NetworkSelect {
 	Testnet = NetworkSelectOption(fmt.Sprintf("Testnet (%s)", utils.GetConfig("constants.chain_id.testnet")))
 	Mainnet = NetworkSelectOption(fmt.Sprintf("Mainnet (%s)", utils.GetConfig("constants.chain_id.mainnet")))
 	return &NetworkSelect{
 		Selector: utils.Selector[NetworkSelectOption]{
 			Options: []NetworkSelectOption{
 				Testnet,
-				Mainnet,
+				//Mainnet,
 			},
 		},
 		state:    state,
@@ -156,7 +157,8 @@ func (m *NetworkSelect) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m *NetworkSelect) View() string {
-	return m.state.weave.Render() + styles.RenderPrompt(
+	return styles.Text("🪢 For launching Minitia, once all required configurations are complete, \nit will run for a few blocks to set up neccesary components.\nPlease note that this may take a moment, and your patience is appreciated!\n\n", styles.Ivory) +
+		m.state.weave.Render() + styles.RenderPrompt(
 		m.GetQuestion(),
 		[]string{"Initia L1 network"},
 		styles.Question,
@@ -248,9 +250,10 @@ func (m *VersionSelect) Init() tea.Cmd {
 func (m *VersionSelect) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	selected, cmd := m.Select(msg)
 	if selected != nil {
-		// TODO: Set the selected version
-		m.state.weave.PushPreviousResponse(styles.RenderPreviousResponse(styles.DotsSeparator, m.GetQuestion(), []string{"minitiad version"}, ""))
-		return m, nil
+		m.state.minitiadVersion = *selected
+		m.state.minitiadEndpoint = m.versions[*selected]
+		m.state.weave.PushPreviousResponse(styles.RenderPreviousResponse(styles.DotsSeparator, m.GetQuestion(), []string{"minitiad version"}, *selected))
+		return NewChainIdInput(m.state), nil
 	}
 
 	return m, cmd
@@ -258,4 +261,330 @@ func (m *VersionSelect) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m *VersionSelect) View() string {
 	return m.state.weave.Render() + styles.RenderPrompt(m.GetQuestion(), []string{"minitiad version"}, styles.Question) + m.Selector.View()
+}
+
+type ChainIdInput struct {
+	utils.TextInput
+	state    *LaunchState
+	question string
+}
+
+func NewChainIdInput(state *LaunchState) *ChainIdInput {
+	model := &ChainIdInput{
+		TextInput: utils.NewTextInput(),
+		state:     state,
+		question:  "Please specify the L2 chain id",
+	}
+	model.WithPlaceholder("Enter in alphanumeric format")
+	return model
+}
+
+func (m *ChainIdInput) GetQuestion() string {
+	return m.question
+}
+
+func (m *ChainIdInput) Init() tea.Cmd {
+	return nil
+}
+
+func (m *ChainIdInput) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	input, cmd, done := m.TextInput.Update(msg)
+	if done {
+		m.state.chainId = input.Text
+		m.state.weave.PushPreviousResponse(styles.RenderPreviousResponse(styles.DotsSeparator, m.GetQuestion(), []string{"L2 chain id"}, input.Text))
+		return NewGasDenomInput(m.state), nil
+	}
+	m.TextInput = input
+	return m, cmd
+}
+
+func (m *ChainIdInput) View() string {
+	return m.state.weave.Render() + styles.RenderPrompt(m.GetQuestion(), []string{"L2 chain id"}, styles.Question) + m.TextInput.View()
+}
+
+type GasDenomInput struct {
+	utils.TextInput
+	state    *LaunchState
+	question string
+}
+
+func NewGasDenomInput(state *LaunchState) *GasDenomInput {
+	model := &GasDenomInput{
+		TextInput: utils.NewTextInput(),
+		state:     state,
+		question:  "Please specify the L2 Gas Token Denom",
+	}
+	model.WithPlaceholder("Enter the denom")
+	return model
+}
+
+func (m *GasDenomInput) GetQuestion() string {
+	return m.question
+}
+
+func (m *GasDenomInput) Init() tea.Cmd {
+	return nil
+}
+
+func (m *GasDenomInput) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	input, cmd, done := m.TextInput.Update(msg)
+	if done {
+		m.state.gasDenom = input.Text
+		m.state.weave.PushPreviousResponse(styles.RenderPreviousResponse(styles.DotsSeparator, m.GetQuestion(), []string{"L2 Gas Token Denom"}, input.Text))
+		return NewMonikerInput(m.state), nil
+	}
+	m.TextInput = input
+	return m, cmd
+}
+
+func (m *GasDenomInput) View() string {
+	return m.state.weave.Render() + styles.RenderPrompt(m.GetQuestion(), []string{"L2 Gas Token Denom"}, styles.Question) + m.TextInput.View()
+}
+
+type MonikerInput struct {
+	utils.TextInput
+	state    *LaunchState
+	question string
+}
+
+func NewMonikerInput(state *LaunchState) *MonikerInput {
+	model := &MonikerInput{
+		TextInput: utils.NewTextInput(),
+		state:     state,
+		question:  "Please specify the moniker",
+	}
+	model.WithPlaceholder("Enter the moniker")
+	return model
+}
+
+func (m *MonikerInput) GetQuestion() string {
+	return m.question
+}
+
+func (m *MonikerInput) Init() tea.Cmd {
+	return nil
+}
+
+func (m *MonikerInput) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	input, cmd, done := m.TextInput.Update(msg)
+	if done {
+		m.state.moniker = input.Text
+		m.state.weave.PushPreviousResponse(styles.RenderPreviousResponse(styles.DotsSeparator, m.GetQuestion(), []string{"moniker"}, input.Text))
+		return NewAddGenesisAccountsSelect(false, m.state), nil
+	}
+	m.TextInput = input
+	return m, cmd
+}
+
+func (m *MonikerInput) View() string {
+	return m.state.weave.Render() + styles.RenderPrompt(m.GetQuestion(), []string{"moniker"}, styles.Question) + m.TextInput.View()
+}
+
+type AddGenesisAccountsSelect struct {
+	utils.Selector[AddGenesisAccountsOption]
+	state             *LaunchState
+	recurring         bool
+	firstTimeQuestion string
+	recurringQuestion string
+}
+
+type AddGenesisAccountsOption string
+
+const (
+	Yes AddGenesisAccountsOption = "Yes"
+	No  AddGenesisAccountsOption = "No"
+)
+
+func NewAddGenesisAccountsSelect(recurring bool, state *LaunchState) *AddGenesisAccountsSelect {
+	if !recurring {
+		state.preGenesisAccountsResponsesCount = len(state.weave.PreviousResponse)
+	}
+	return &AddGenesisAccountsSelect{
+		Selector: utils.Selector[AddGenesisAccountsOption]{
+			Options: []AddGenesisAccountsOption{
+				Yes,
+				No,
+			},
+		},
+		state:             state,
+		recurring:         recurring,
+		firstTimeQuestion: "Would you like to add genesis accounts?",
+		recurringQuestion: "Would you like to add another genesis account?",
+	}
+}
+
+func (m *AddGenesisAccountsSelect) GetQuestionAndHighlight() (string, string) {
+	if m.recurring {
+		return m.recurringQuestion, "genesis account"
+	}
+	return m.firstTimeQuestion, "genesis accounts"
+}
+
+func (m *AddGenesisAccountsSelect) Init() tea.Cmd {
+	return nil
+}
+
+func (m *AddGenesisAccountsSelect) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	selected, cmd := m.Select(msg)
+	if selected != nil {
+		switch *selected {
+		case Yes:
+			question, highlight := m.GetQuestionAndHighlight()
+			m.state.weave.PushPreviousResponse(styles.RenderPreviousResponse(styles.ArrowSeparator, question, []string{highlight}, string(*selected)))
+			return NewGenesisAccountsAddressInput(m.state), nil
+		case No:
+			question := m.firstTimeQuestion
+			highlight := "genesis accounts"
+			if len(m.state.genesisAccounts) > 0 {
+				m.state.weave.PreviousResponse = m.state.weave.PreviousResponse[:m.state.preGenesisAccountsResponsesCount]
+				m.state.weave.PushPreviousResponse(styles.RenderPreviousResponse(styles.ArrowSeparator, question, []string{highlight}, string(Yes)))
+				currentResponse := "  List of the Genesis Accounts\n"
+				for _, account := range m.state.genesisAccounts {
+					currentResponse += styles.Text(fmt.Sprintf("  %s\tInitial Balance: %s\n", account.address, account.balance), styles.Gray)
+				}
+				m.state.weave.PushPreviousResponse(currentResponse)
+			} else {
+				m.state.weave.PushPreviousResponse(styles.RenderPreviousResponse(styles.ArrowSeparator, question, []string{highlight}, string(No)))
+			}
+			return NewOpBridgeSubmissionIntervalInput(m.state), nil
+		}
+	}
+
+	return m, cmd
+}
+
+func (m *AddGenesisAccountsSelect) View() string {
+	preText := ""
+	if !m.recurring {
+		preText += "\n" + styles.RenderPrompt("You can add genesis accounts by first entering the addresses, then assigning the initial balance one by one.", []string{"genesis accounts"}, styles.Information) + "\n"
+	}
+	question, highlight := m.GetQuestionAndHighlight()
+	return m.state.weave.Render() + preText + styles.RenderPrompt(
+		question,
+		[]string{highlight},
+		styles.Question,
+	) + m.Selector.View()
+}
+
+type GenesisAccountsAddressInput struct {
+	utils.TextInput
+	state    *LaunchState
+	question string
+}
+
+func NewGenesisAccountsAddressInput(state *LaunchState) *GenesisAccountsAddressInput {
+	model := &GenesisAccountsAddressInput{
+		TextInput: utils.NewTextInput(),
+		state:     state,
+		question:  "Please specify genesis account address",
+	}
+	model.WithPlaceholder("Enter the address")
+	return model
+}
+
+func (m *GenesisAccountsAddressInput) GetQuestion() string {
+	return m.question
+}
+
+func (m *GenesisAccountsAddressInput) Init() tea.Cmd {
+	return nil
+}
+
+func (m *GenesisAccountsAddressInput) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	input, cmd, done := m.TextInput.Update(msg)
+	if done {
+		m.state.weave.PushPreviousResponse(styles.RenderPreviousResponse(styles.DotsSeparator, m.GetQuestion(), []string{"genesis account address"}, input.Text))
+		return NewGenesisAccountsBalanceInput(input.Text, m.state), nil
+	}
+	m.TextInput = input
+	return m, cmd
+}
+
+func (m *GenesisAccountsAddressInput) View() string {
+	return m.state.weave.Render() + styles.RenderPrompt(m.GetQuestion(), []string{"moniker"}, styles.Question) + m.TextInput.View()
+}
+
+type GenesisAccountsBalanceInput struct {
+	utils.TextInput
+	state    *LaunchState
+	address  string
+	question string
+}
+
+func NewGenesisAccountsBalanceInput(address string, state *LaunchState) *GenesisAccountsBalanceInput {
+	model := &GenesisAccountsBalanceInput{
+		TextInput: utils.NewTextInput(),
+		state:     state,
+		address:   address,
+		question:  fmt.Sprintf("Please specify initial balance for %s (%s)", address, state.gasDenom),
+	}
+	model.WithPlaceholder("Enter the desired balance")
+	// TODO: Maybe Coin validate here
+	return model
+}
+
+func (m *GenesisAccountsBalanceInput) GetQuestion() string {
+	return m.question
+}
+
+func (m *GenesisAccountsBalanceInput) Init() tea.Cmd {
+	return nil
+}
+
+func (m *GenesisAccountsBalanceInput) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	input, cmd, done := m.TextInput.Update(msg)
+	if done {
+		m.state.genesisAccounts = append(m.state.genesisAccounts, GenesisAccount{
+			address: m.address,
+			balance: input.Text,
+		})
+		m.state.weave.PushPreviousResponse(styles.RenderPreviousResponse(styles.DotsSeparator, m.GetQuestion(), []string{m.address}, input.Text))
+		return NewAddGenesisAccountsSelect(true, m.state), nil
+	}
+	m.TextInput = input
+	return m, cmd
+}
+
+func (m *GenesisAccountsBalanceInput) View() string {
+	return m.state.weave.Render() + styles.RenderPrompt(m.GetQuestion(), []string{m.address}, styles.Question) + m.TextInput.View()
+}
+
+type OpBridgeSubmissionIntervalInput struct {
+	utils.TextInput
+	state    *LaunchState
+	question string
+}
+
+func NewOpBridgeSubmissionIntervalInput(state *LaunchState) *OpBridgeSubmissionIntervalInput {
+	model := &OpBridgeSubmissionIntervalInput{
+		TextInput: utils.NewTextInput(),
+		state:     state,
+		question:  "Please specify OP bridge config: Submission Interval (format m, h or d - ex. 1m, 23h, 7d)",
+	}
+	model.WithPlaceholder("Press tab to use “1m”")
+	return model
+}
+
+func (m *OpBridgeSubmissionIntervalInput) GetQuestion() string {
+	return m.question
+}
+
+func (m *OpBridgeSubmissionIntervalInput) Init() tea.Cmd {
+	return nil
+}
+
+func (m *OpBridgeSubmissionIntervalInput) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	input, cmd, done := m.TextInput.Update(msg)
+	if done {
+		// TODO: Continue flow
+		m.state.opBridgeSubmissionInterval = input.Text
+		m.state.weave.PushPreviousResponse(styles.RenderPreviousResponse(styles.DotsSeparator, m.GetQuestion(), []string{"Submission Interval"}, input.Text))
+		return m, tea.Quit
+	}
+	m.TextInput = input
+	return m, cmd
+}
+
+func (m *OpBridgeSubmissionIntervalInput) View() string {
+	return m.state.weave.Render() + styles.RenderPrompt(m.GetQuestion(), []string{"Submission Interval"}, styles.Question) + m.TextInput.View()
 }
--- models/minitia/state.go
+++ models/minitia/state.go
@@ -2,11 +2,24 @@ package minitia
 
 import "github.com/initia-labs/weave/types"
 
+type GenesisAccount struct {
+	address string
+	balance string
+}
+
 type LaunchState struct {
-	weave              types.WeaveState
-	existingMinitiaApp bool
-	l1Network          string
-	vmType             string
+	weave                            types.WeaveState
+	existingMinitiaApp               bool
+	l1Network                        string
+	vmType                           string
+	minitiadVersion                  string
+	minitiadEndpoint                 string
+	chainId                          string
+	gasDenom                         string
+	moniker                          string
+	genesisAccounts                  []GenesisAccount
+	preGenesisAccountsResponsesCount int
+	opBridgeSubmissionInterval       string
 }
 
 func NewLaunchState() *LaunchState {
