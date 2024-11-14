746ada94ccb0049ebcb1a89b9df66a03f3b9e5e1
beeb@neb.one <mercedesmwit20@gmail.com>
2024-11-14 13:25:53 +0700

    feat: recover from panic cause

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/initia-labs/weave/cmd"
 )
 
 func main() {
-	if err := cmd.Execute(); err != nil {
+	if err := executeWithRecovery(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func executeWithRecovery() (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			// Print a clean error message to stderr on panic
+			fmt.Fprintln(os.Stderr, "An unexpected error occurred:", r)
+			err = fmt.Errorf("%v", r)
+		}
+	}()
+
+	// Execute the main command
+	return cmd.Execute()
 }
