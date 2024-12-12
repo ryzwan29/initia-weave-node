7141ecbc49aa76fbbd79a824fc130037005dafdc
beeb@neb.one <mercedesmwit20@gmail.com>
2024-12-12 16:31:17 +0700

    feat: setup cosmovisor

--- models/initia/run_l1_node.go
+++ models/initia/run_l1_node.go
@@ -935,12 +935,13 @@ func initializeApp(ctx context.Context) tea.Cmd {
 			if err := runCmd.Run(); err != nil {
 				panic(fmt.Sprintf("failed to run initiad init: %v", err))
 			}
-			runCmd = exec.Command(cosmovisorPath, "init", binaryPath)
-			runCmd.Env = append(runCmd.Env, "DAEMON_NAME=initiad", "DAEMON_HOME="+initiaHome)
-			if err := runCmd.Run(); err != nil {
-				panic(fmt.Sprintf("failed to run cosmovisor init: %v", err))
-			}
 
+		}
+
+		runCmd := exec.Command(cosmovisorPath, "init", binaryPath)
+		runCmd.Env = append(runCmd.Env, "DAEMON_NAME=initiad", "DAEMON_HOME="+initiaHome)
+		if err := runCmd.Run(); err != nil {
+			panic(fmt.Sprintf("failed to run cosmovisor init: %v", err))
 		}
 
 		err = io.CopyDirectory(filepath.Dir(binaryPath), filepath.Join(initiaHome, "cosmovisor", "current"))
