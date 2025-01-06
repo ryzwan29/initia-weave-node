ebb6d042d2555b6594a645c35698193a10c1d536
Wasin Watthanasrisong <wasinwatt@gmail.com>
2025-01-06 18:29:56 +0700

    feat: add faucet link to gas-station show and fix gas station bug

--- cmd/gas_station_integration_test.go
+++ cmd/gas_station_integration_test.go
@@ -4,18 +4,34 @@
 package cmd_test
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/initia-labs/weave/common"
 	"github.com/initia-labs/weave/models"
 	"github.com/initia-labs/weave/testutil"
+	"github.com/stretchr/testify/assert"
 )
 
+const weaveDirectoryBackup = ".weave_back"
+
 func TestGasStationSetup(t *testing.T) {
+	// if Weave home already exists, copy the content somewhere else and replace it again after the test
+	userHome, _ := os.UserHomeDir()
+	weaveDir := filepath.Join(userHome, common.WeaveDirectory)
+	weaveDirBackup := filepath.Join(userHome, weaveDirectoryBackup)
+	if _, err := os.Stat(weaveDir); !os.IsNotExist(err) {
+		// remove the backup directory if it exists
+		fmt.Println("Removing backup directory")
+		os.RemoveAll(weaveDirBackup)
+		// rename the weave directory to backup
+		if err := os.Rename(weaveDir, weaveDirBackup); err != nil {
+			t.Fatalf("Failed to backup weave directory: %v", err)
+		}
+	}
+
 	finalModel := testutil.SetupGasStation(t)
 
 	// Check the final state here
@@ -26,12 +42,17 @@ func TestGasStationSetup(t *testing.T) {
 	}
 
 	// Check if Weave home has been created
-	userHome, _ := os.UserHomeDir()
-	weaveDir := filepath.Join(userHome, common.WeaveDirectory)
 	_, err := os.Stat(weaveDir)
 	assert.Nil(t, err)
 
 	// Assert values
 	weaveConfig := filepath.Join(weaveDir, "config.json")
 	testutil.CompareJsonValue(t, weaveConfig, "common.gas_station_mnemonic", testutil.GasStationMnemonic)
+
+	// Restore the Weave home directory
+	if _, err := os.Stat(weaveDirBackup); !os.IsNotExist(err) {
+		fmt.Println("Restoring Weave home directory")
+		os.RemoveAll(weaveDir)
+		os.Rename(weaveDirBackup, weaveDir)
+	}
 }
