b06c2418ef8b7f2d7ad788091fb962aaa34d2f39
traviolus <42636319+traviolus@users.noreply.github.com>
2025-01-16 10:51:16 +0700

    feat(relayer): integration tests

--- cmd/helper_integration_test.go
+++ cmd/helper_integration_test.go
@@ -13,7 +13,11 @@ import (
 	"github.com/initia-labs/weave/common"
 )
 
-const weaveDirectoryBackup = ".weave_backup"
+const (
+	weaveDirectoryBackup  = ".weave_backup"
+	hermesDirectory       = ".hermes"
+	hermesDirectoryBackup = ".hermes_backup"
+)
 
 func setup() {
 	// disable analytics
@@ -25,11 +29,24 @@ func setup() {
 	if _, err := os.Stat(weaveDir); !os.IsNotExist(err) {
 		// remove the backup directory if it exists
 		os.RemoveAll(weaveDirBackup)
-		// rename the weave directory to backup
-		fmt.Println("Backuping weave directory")
+		// rename the weave directory to back up
+		fmt.Println("Backing up weave directory")
 
 		if err := os.Rename(weaveDir, weaveDirBackup); err != nil {
 			panic(fmt.Errorf("failed to backup weave directory: %v", err))
+		}
+	}
+
+	relayerDir := filepath.Join(userHome, hermesDirectory)
+	relayerDirBackup := filepath.Join(userHome, hermesDirectoryBackup)
+	if _, err := os.Stat(relayerDir); !os.IsNotExist(err) {
+		// remove the backup directory if it exists
+		os.RemoveAll(relayerDirBackup)
+		// rename the hermes directory to back up
+		fmt.Println("Backing up hermes directory")
+
+		if err := os.Rename(relayerDir, relayerDirBackup); err != nil {
+			panic(fmt.Errorf("failed to backup hermes directory: %v", err))
 		}
 	}
 }
@@ -42,6 +59,14 @@ func teardown() {
 		fmt.Println("Restoring weave directory")
 		os.RemoveAll(weaveDir)
 		os.Rename(weaveDirBackup, weaveDir)
+	}
+
+	relayerDir := filepath.Join(userHome, hermesDirectory)
+	relayerDirBackup := filepath.Join(userHome, hermesDirectoryBackup)
+	if _, err := os.Stat(relayerDirBackup); !os.IsNotExist(err) {
+		fmt.Println("Restoring hermes directory")
+		os.RemoveAll(relayerDirBackup)
+		os.Rename(relayerDir, relayerDirBackup)
 	}
 }
 
